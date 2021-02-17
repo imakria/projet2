a92fb1f9d27e756367ed70d8bcff76975b99dea0
imakria <imakria@gmail.com>
2021-02-17 16:18:57 +0100

    structure page ville avec formulaire, controller, twig

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Ville;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VilleController extends AbstractController
 {
     /**
+     * //     * @IsGranted("ROLE_ADMIN")
      * @Route("/ville", name="ville")
+     * @param Request $request
+     * @param VilleRepository $villeRepository
+     * @param EntityManagerInterface $em
+     * @return Response
      */
-    public function index(): Response
+    public function allCities(Request $request, VilleRepository $villeRepository, EntityManagerInterface $em): Response
     {
-        return $this->render('ville/index.html.twig', [
+
+        $ville = new Ville();
+        $villes = $villeRepository->findAll();
+
+
+
+        return $this->render('ville/ville.html.twig', [
             'controller_name' => 'VilleController',
         ]);
     }
